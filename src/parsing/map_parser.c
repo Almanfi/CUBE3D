e4d5d31a385c25e575e47ee7c1061d58450c8742
Brahim Amrouch <amroucheb4@gmail.com>
2023-07-03 19:05:35 +0100

    map parser working

--- src/parsing/map_parser.c
+++ src/parsing/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 17:13:43 by bamrouch          #+#    #+#             */
-/*   Updated: 2023/07/03 12:44:19 by bamrouch         ###   ########.fr       */
+/*   Updated: 2023/07/03 19:02:39 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,18 @@
 static  void    skip_empty_lines(t_cub3d *cub3d)
 {
     char *line;
-    t_boolean   start_wall;
 
-    start_wall = FALSE;
-    while (*cub3d->map_content && !start_wall)
+    while (*cub3d->map_content)
     {
         line = skip_space(*cub3d->map_content);
-        while (*line && (*line == '1' || ft_is_space(*line)))
-        {
-            line++;
-            if (!*line)
-                start_wall = TRUE;
-        }
         if (*line)
-            exit_cub3d(-1, "surround map with walls");
-        if (!start_wall)
         {
-            ft_free_node(1, *cub3d->map_content);
-            cub3d->map_content++;
+            printf("broke here  |%s|\n", line);
+            cub3d->map_content--;
+            break;
         }
+        cub3d->map_content++;
     }
-    if (!start_wall)
-        exit_cub3d(-1, "no map given");
 }
 
 static  void    fill_short_lines(size_t max_len, t_cub3d *cub3d)
@@ -83,44 +73,67 @@ static  void    justify_lines(t_cub3d *cub3d)
     fill_short_lines(max_len, cub3d);
 }   
 
+static t_boolean is_start_pos(char c)
+{
+    if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
+        return TRUE;
+    return FALSE;
+}
+
+static  t_boolean open_wall(char **map, size_t i, size_t j)
+{
+    if (ft_is_space(map[i - 1][j - 1]) || ft_is_space(map[i - 1][j]) || ft_is_space(map[i - 1][j + 1])
+        || ft_is_space(map[i][j - 1]) || ft_is_space(map[i][j + 1]) ||
+        ft_is_space(map[i + 1][j - 1]) || ft_is_space(map[i + 1][j]) || ft_is_space(map[i + 1][j + 1]))
+        return TRUE;
+    return FALSE;
+}
 
 void    map_parser(t_cub3d *cub3d)
 {
     char    **map;
+    char    *line;
     size_t  i;
     size_t  j;
 
     skip_empty_lines(cub3d);
+    if (!*cub3d->map_content)
+        exit_cub3d(-1, "no map given");
     justify_lines(cub3d);
-
     i = 1;
     map = cub3d->map_content;
     while (map[i])
     {
         j = 0;
-        printf("|%s|\n", map[i]);
+        line = skip_space(map[i]);
+        printf("the line |%s|\n", line);
+        if (!*line)
+            break;
         while (map[i][j])
         {
             if (map[i][j] == '1' || ft_is_space(map[i][j]))
                 ;
-            else if (map[i][j] == '0' && (j == 0 ||
-                ft_is_space(map[i - 1][j - 1]) || ft_is_space(map[i - 1][j]) || ft_is_space(map[i - 1][j + 1]) ||
-                ft_is_space(map[i][j - 1]) || ft_is_space(map[i][j + 1]) ||
-                ft_is_space(map[i + 1][j - 1]) || ft_is_space(map[i + 1][j]) || ft_is_space(map[i + 1][j + 1])))
+            else if ((map[i][j] == '0' ||  is_start_pos(map[i][j])) && (j == 0 || open_wall(map, i, j)))
                 exit_cub3d(-1, "unvalid map due to unclosed walls");
-            else if (map[i][j] == 'N' || map[i][j] == 'E' || map[i][j] == 'S' || map[i][j] == 'W')
+            else if (is_start_pos(map[i][j]))
             {
                 if (cub3d->player_set)
                     exit_cub3d(-1, "unvalid map due to mutiple player positions");
                 else
                     cub3d->player_set = TRUE;
             }
-            else
+            else if (map[i][j] != '0')
                 exit_cub3d(-1, "wrong map caracter");
             j++;
-            printf("the |%c|\n", map[i][j]);
         }
         i++;
     }
+    if (!cub3d->player_set)
+        exit_cub3d(-1, "starting player position is mandatory");
+    cub3d->map_content += i;
+    skip_empty_lines(cub3d);
+    printf("the finale line |%s|\n", *cub3d->map_content);
+    if (*cub3d->map_content)
+        exit_cub3d(-1, "garbage value after map");
 }
 
