42684c8c0933158c558e060c507c4d0e7a3d6483
Almanfi <nassimed.dev@gmail.com>
2023-09-07 01:02:32 +0100

    texture for everyone

--- src/utils/texture_reader.c
+++ src/utils/texture_reader.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   texture_reader.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: maboulkh <maboulkh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:47:32 by bamrouch          #+#    #+#             */
-/*   Updated: 2023/09/04 19:48:23 by bamrouch         ###   ########.fr       */
+/*   Updated: 2023/09/06 21:35:42 by maboulkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 void	store_texture(t_cub3d *cub3d, t_tx_type tx_side, void *img)
 {
+	double			step_x;
+	double			step_y;
 	int				x;
 	int				y;
 	t_frame_data	texture_data;
 
 	texture_data.addr = mlx_get_data_addr(img, &texture_data.bits_per_pixel,
 			&texture_data.line_length, &texture_data.endian);
+	step_x = (double )TEX_DIMENSIONS / cub3d->img_width;
+	step_y = (double )TEX_DIMENSIONS / cub3d->img_height;
 	x = 0;
-	while (x < TEX_DIMENSIONS)
+	while (x < (int )(cub3d->img_width * step_x))
 	{
 		y = 0;
-		while (y < TEX_DIMENSIONS)
+		while (y < (int )(cub3d->img_height * step_y))
 		{
 			cub3d->texture.tx[tx_side][TEX_DIMENSIONS * y
 				+ x] = ((int *)texture_data.addr)[(texture_data.line_length / 4)
-				* y + x];
+				* (int )(y / step_y) + (int )(x / step_x)];
 			y++;
 		}
 		x++;
